e698c2546996fbe60de9e334359ecd2d77514575
Basit ali <basitalyshah51214@gmail.com>
2023-11-16 10:14:36 -0800

    changes to add theme of the app

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,28 @@
+import 'package:citta_db/provider/dark_theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final themeState = Provider.of<DarkThemeProvider>(context);
+    return Scaffold(
+      body: Center(
+        child: SwitchListTile(
+          title: const Text("Theme"),
+          secondary: Icon(
+            themeState.getDarkTheme
+                ? Icons.dark_mode_outlined
+                : Icons.light_mode_outlined,
+          ),
+          onChanged: (bool value) {
+            themeState.setDarkTheme = value;
+          },
+          value: themeState.getDarkTheme,
+        ),
+      ),
+    );
   }
 }
