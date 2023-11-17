82ef69ba64586d22d1432541c3821c61bd3992e1
Basit ali <basitalyshah51214@gmail.com>
2023-11-17 10:33:22 -0800

    user screen complete

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -1,3 +1,4 @@
+import 'package:citta_db/widgets/text_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -14,73 +15,101 @@ class _UserScreenState extends State<UserScreen> {
   @override
   Widget build(BuildContext context) {
     final themeState = Provider.of<DarkThemeProvider>(context);
-
+    final Color color = themeState.getDarkTheme ? Colors.white : Colors.black54;
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            // ListTile(
-            //   leading: Icon(
-            //     Icons.person,
-            //   ),
-            //   title: Text(
-            //     "Address",
-            //     style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-            //   ),
-            //   subtitle: Text("Sub-Title-Here"),
-            //   trailing: Icon(
-            //     Icons.navigate_next,
-            //   ),
-            // ),
-            _listTile(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: RichText(
+                  text: TextSpan(
+                      text: "Hi,  ",
+                      style: const TextStyle(
+                          color: Colors.cyan,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: "My Name",
+                          style: TextStyle(
+                              color: color,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ]),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 8.0,
+                  top: 8.0,
+                ),
+                child: TextWidget(
+                    text: "Example@gmail.com", color: color, textSize: 14),
+              ),
+              const Divider(
+                thickness: 2,
+              ),
+              _listTile(
                 icon: Icons.person_outline,
                 onTap: () {},
                 title: "Address",
-                subTitle: "My Address"),
-            _listTile(
-              icon: Icons.account_balance_wallet_rounded,
-              onTap: () {},
-              title: "Orders",
-            ),
-            _listTile(
-              icon: Icons.favorite_border,
-              onTap: () {},
-              title: "WishList",
-            ),
-            _listTile(
-              icon: Icons.remove_red_eye_outlined,
-              onTap: () {},
-              title: "Viewed",
-            ),
-            _listTile(
-              icon: Icons.lock_outline_rounded,
-              onTap: () {},
-              title: "Forget Password",
-            ),
-            SwitchListTile(
-              title: const Text(
-                "Theme",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                subTitle: "My Address",
+                color: color,
               ),
-              secondary: Icon(
-                themeState.getDarkTheme
-                    ? Icons.dark_mode_outlined
-                    : Icons.light_mode_outlined,
+              _listTile(
+                icon: Icons.account_balance_wallet_rounded,
+                onTap: () {},
+                title: "Orders",
+                color: color,
               ),
-              onChanged: (bool value) {
-                setState(() {
-                  themeState.setDarkTheme = value;
-                });
-              },
-              value: themeState.getDarkTheme,
-            ),
-
-            _listTile(
-              icon: Icons.logout,
-              onTap: () {},
-              title: " Logout",
-            ),
-          ],
+              _listTile(
+                icon: Icons.favorite_border,
+                onTap: () {},
+                title: "WishList",
+                color: color,
+              ),
+              _listTile(
+                icon: Icons.remove_red_eye_outlined,
+                onTap: () {},
+                title: "Viewed",
+                color: color,
+              ),
+              _listTile(
+                icon: Icons.lock_outline_rounded,
+                onTap: () {},
+                title: "Forget Password",
+                color: color,
+              ),
+              SwitchListTile(
+                title: const Text(
+                  "Theme",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                ),
+                secondary: Icon(
+                  themeState.getDarkTheme
+                      ? Icons.dark_mode_outlined
+                      : Icons.light_mode_outlined,
+                ),
+                onChanged: (bool value) {
+                  setState(() {
+                    themeState.setDarkTheme = value;
+                  });
+                },
+                value: themeState.getDarkTheme,
+              ),
+              _listTile(
+                icon: Icons.logout,
+                onTap: () {},
+                title: " Logout",
+                color: color,
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -90,7 +119,8 @@ class _UserScreenState extends State<UserScreen> {
       {required String title,
       String? subTitle,
       required IconData icon,
-      required Function onTap}) {
+      required Function onTap,
+      required Color color}) {
     return InkWell(
       onTap: onTap(),
       child: ListTile(
@@ -101,7 +131,11 @@ class _UserScreenState extends State<UserScreen> {
           title,
           style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
         ),
-        subtitle: Text(subTitle ?? ""),
+        subtitle: TextWidget(
+          text: subTitle ?? "",
+          color: color,
+          textSize: 14,
+        ),
         trailing: const Icon(
           Icons.navigate_next,
         ),
--- lib/widgets/text_widgets.dart
+++ lib/widgets/text_widgets.dart
@@ -1,10 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first, must_be_immutable
+
 import 'package:flutter/material.dart';
 
 class TextWidget extends StatelessWidget {
-  const TextWidget({super.key});
-
+  TextWidget({
+    Key? key,
+    required this.text,
+    required this.color,
+    required this.textSize,
+    this.isText = false,
+    this.maxLine = 10,
+  }) : super(key: key);
+  final String text;
+  final Color color;
+  final double textSize;
+  bool isText;
+  int maxLine = 10;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Text(
+      text,
+      maxLines: maxLine,
+      style: TextStyle(
+        overflow: TextOverflow.ellipsis,
+        color: color,
+        fontSize: textSize,
+        fontWeight: isText ? FontWeight.w600 : FontWeight.w400,
+      ),
+    );
   }
 }
